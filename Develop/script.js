89d2ce9b1ab7dca9b84519182fdf06d83d8b1d78
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-11-01 21:31:44 -0500

    update number, letter, special char, funtion write password

--- Develop/script.js
+++ Develop/script.js
@@ -1,129 +1,95 @@
 // Assignment code here
 
 
-var lowerCase = ["abcdefghijklmnop"];
-var upperCase = ["lowerCase.toUpperCase()"];
-var numbers = ["0123456789"];
-var specialChar = ["!@#$%^&*()=+?/"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","z","y","z"];
+var upperCase = ["A","B","C","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var number = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","@","#","$","%","^","&","*","(",")"];
 
 
 
-
+//document.getElementById("password")
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var button = document.getElementById("generate");
 
-// Write password to the #password input
-function writePassword() {
-  
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+var userPick = {};
 
 
 // prompt that come after you generate password 
 
-  var passwordLength = prompt ("Please enter the number of characters you want for you password. Can be 8 characters and no more that 128 characters");
-  if (passwordLength >= 8 || passwordLength <=128)
-      var newPasswordLength = parseInt(passwordLength)
+  var passwordLength = parseInt(prompt("Please enter the number of characters you want for you password. Can be 8 characters and no more that 128 characters"));
 
+  console.log(passwordLength)
 
+  function promptUser(){
   var numbers = confirm ("Would you like number in your password?");
   
-  var lowerCase = confirm ("Would you like lowerCase in your password?");
+  var lowerCases = confirm ("Would you like lowerCase in your password?");
 
-  var upperCase = confirm ("Would you like uppercase in your password?");
+  var upperCases = confirm ("Would you like uppercase in your password?");
 
-  var specialChar = confirm ("Would you like special characters in your password");
+  var specialChars = confirm ("Would you like special characters in your password");
 
-  while (upperCase === false && lowerCase === false && specialChar === false && numbers === false) {
-    alert("You must choose at least one parameter");
-      
-  var numbers = confirm ("Would you like number in your password?");
-  
-  var lowerCase = confirm ("Would you like lowerCase in your password?");
 
-  var upperCase = confirm ("Would you like uppercase in your password?");
-
-  var specialChar = confirm ("Would you like special characters in your password");
   
  
-   // Minimum count for numbers, lowerCase, UpperCase, and special 
-  var minimumCount = 0;
+  if (numbers) {
+    userPick ["number"] = number;
 
-
-  var minimumnumber = "";
-
-  var minimumlowerCase = "";
-
-  var minimumupperCase = "";
-
-  var minimumspecialChar = "";
-
-
-
-
-  var functionArray = {
-    getnumbers: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-    },
-
-    getlowerCases: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-    },
-
-    getupperCases: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-    },
-
-    getspecialChar: function() {
-      return specialChar[Math.floor(Math.random() * specialChar.length)]
-    }
-
-};
- 
-console.log()
-
-if (numbers === true) {
-  minimumnumber = functionArray.getnumber();
-  minimumCount++;
-}
-
-if (upperCase === true) {
-  minimumupperCase = functionArray.getupperCase();
-  minimumCount++;
-  
-}
-
-if (lowerCase === true) {
-  minimumlowerCase = functionArray.getlowerCase();
-  minimumCount++;
-  
-}
-
-if (specialChar === true) {
-  minimumspecialChar = functionArray.getspecialChar();
-  minimumCount++;
-  
-}
-
-
-
-for (let i = 0; i < ((passwordLength) - minimumCount); i++) {
-  var pass = Math.floor(Math.random() * 4);
-
-  randomPasswordGenerated += randomNumberPicked;
-
-}
-
-document.getElementById("password").value =passwaord;
   }
 
+  if (lowerCases) {
+    userPick["lowerCase"] = lowerCase;
+  }
 
+  if (upperCases) {
+    userPick["upperCase"] = upperCase;
+  }
 
-//alert(password)
+  if (specialChars) {
+    userPick["specialChar"] = specialChar;
+  }
+
+  console.log(userPick);
+  }
+
+promptUser();
+
+  
+// Write password to the #password input
+function writePassword() {
+  let lastPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+
+    let passwordNew = Object.keys(userPick)
+    let random = passwordNew[Math.floor(Math.random() * passwordNew.length)]
+    let charAt = getRandomCharacter(userPick[random]);
+
+    lastPassword += charAt;
+
+    console.log('RandomCharacter', charAt);
+    console.log(lastPassword, lastPassword.length);
+    
+
+  } 
+  return lastPassword
+}
+
+function getRandomCharacter(str) {
+  return str[Math.floor(Math.random() * str.length)]
+}
+
+writePassword();
+
+//document.getElementById("password").value = writePassword();
+// Add event listener to generate button
+button.addEventListener("click", function(){
+  password.value = writePassword();
+})
+
+// Write password to the #password input
+
+ alert(password)
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Password Generator Starter Code
+
+Password Generator Website
+
+
+# Linked
+
+[Github](https://wdavonta.github.io/password-generator/ "Password generator Repo"
+
+[Link] (https://wdavonta.github.io/password-generator/)
+
+
+# About the project
+This project is about making place generate a password to create a strong password that provides greater security.
+
+
+
+![alt text]( "Screenshot of webite")
+
+# Contact Info
+Da'Vonta Williamson
+
+E: <a href="mailto:wdavonta@gmail.com">wdavonta@gmail.com</a>
+  </address>
